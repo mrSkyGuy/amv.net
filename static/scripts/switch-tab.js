168e619bb15367582eff5d8784216b12ea76592f
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-13 15:24:53 +0300

    join sign-up and sign-in in sign-up-in html file. Realize switching between two pages

--- static/scripts/switch-tab.js
+++ static/scripts/switch-tab.js
@@ -1,28 +1,24 @@
-let buttonTab1 = document.querySelectorAll(".button-sign")[0],
-    buttonTab2 = document.querySelectorAll(".button-sign")[1]
+const tabs = document.querySelectorAll('.button-sign')
+const indicator = document.querySelector('.selected-tab-indicator')
+const pages = [
+    document.querySelector('.sign-up'), 
+    document.querySelector('.sign-in')
+]
 
-buttonTab1.addEventListener('click', () => {
-    if (!(buttonTab1.classList.contains("selected-tab"))) {
-        buttonTab1.classList.add("selected-tab")
-        buttonTab2.classList.remove("selected-tab")
+
+tabs.forEach(tab => tab.addEventListener('click', e => {
+    const tab = e.target
+    const tabIndex = Array.prototype.indexOf.call(tab.parentNode.children, tab)
+    const indicatorWidth = getComputedStyle(indicator).getPropertyValue('width')
+    
+    if (tabIndex == 0) {
+        pages[0].classList.add('current-page')
+        pages[1].classList.remove('current-page')
+    } else if (tabIndex == 1) {
+        pages[1].classList.add('current-page')
+        pages[0].classList.remove('current-page')
     }
-})
-buttonTab2.addEventListener('click', () => {
-    if (!(buttonTab2.classList.contains("selected-tab"))) {
-        buttonTab2.classList.add("selected-tab")
-        buttonTab1.classList.remove("selected-tab")
-    }
-})
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     document.querySelectorAll(".button-sign").forEach(
-//         (item) => {
-//             item.addEventListener("click", () => {
-//                 if (item.classList.contains("selected-tab")) item.classList.remove("selected-tab")
-//                 else item.classList.add("selected-tab")
-//                 console.log(item.classList);
-//             })
-//         }
-//     )
-// })
+    indicator.style.transform = `translate3d(calc( ${indicatorWidth} * ${tabIndex} ), 0, 0)`
+    indicator.style.width = `calc(${indicatorWidth} + 2%)`
+    setTimeout(() => indicator.style.width = indicatorWidth, 100)
+}))
