ea8641e4fee9b5843c4b250c11cdaf6cc0cef90f
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-16 12:31:26 +0300

    rewrite sign in form with flask wtf

--- app/data/routes.py
+++ app/data/routes.py
@@ -1,6 +1,7 @@
 from flask import redirect, render_template
 
 from main import app
+from forms.sign_in import SignInForm
 from forms.sign_up import SignUpForm
 
 
@@ -14,9 +15,13 @@ def feed():
     return render_template("feed.html")
 
 
-@app.route("/sign-up-in", methods=['GET', 'POST'])
+@app.route("/sign-up-in", methods=["GET", "POST"])
 def test():
     sign_up_form = SignUpForm()
-    if sign_up_form.validate_on_submit():
+    sign_in_form = SignInForm()
+    if sign_up_form.validate_on_submit() or sign_in_form.validate_on_submit():
         return redirect("/feed")
-    return render_template("sign-up-in.html", sign_up_form=sign_up_form)
+        
+    return render_template(
+        "sign-up-in.html", sign_up_form=sign_up_form, sign_in_form=sign_in_form
+    )
---
+++ app/forms/sign_in.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired
+
+from data.db_session import create_session
+from data.models.users import User
+
+
+class SignInForm(FlaskForm):
+    username_or_email = StringField(
+        "username/email", validators=[DataRequired("You missed the field")]
+    )
+    password = PasswordField(
+        "password", validators=[DataRequired("You missed the field")]
+    )
+    remember_me = BooleanField("remember me")
+
+    submit = SubmitField("Continue")
+
+    def validate_username_or_email(self, username_or_email):
+        is_email = False
+        if '@' in username_or_email.data:
+            is_email = True
+        
+        session = create_session()
+
+        if is_email:
+            user = session.query(User).filter(User.email == username_or_email.data).first()
+            if not user:
+                raise ValidationError("User not found")
+        else:
+            user = session.query(User).filter(User.username == username_or_email.data).first()
+            if not user:
+                raise ValidationError("User not found")
+        
