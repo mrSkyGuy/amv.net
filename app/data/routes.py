d3f3b7731781960cc9676924c3970322fd17d1da
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-16 14:38:17 +0300

    rewrite url via url_for function

--- app/data/routes.py
+++ app/data/routes.py
@@ -1,4 +1,4 @@
-from flask import redirect, render_template, request
+from flask import redirect, render_template, url_for
 
 from main import app, login_user
 from data.db_session import create_session
@@ -9,7 +9,7 @@ from forms.sign_up import SignUpForm
 
 @app.route("/")
 def root():
-    return redirect("/feed")
+    return redirect(url_for("feed"))
 
 
 @app.route("/feed")
@@ -17,7 +17,7 @@ def feed():
     return render_template("feed.html")
 
 
-@app.route("/sign-up-in", methods=["GET", "POST"])
+@app.route("/sign_up_in", methods=["GET", "POST"])
 def sign_up_in():
     sign_up_form = SignUpForm()
     sign_in_form = SignInForm()
@@ -32,17 +32,17 @@ def sign_up_in():
         session.add(user)
         session.commit()
 
-        return redirect("/feed")
+        return redirect(url_for("feed"))
 
     if sign_in_form.validate_on_submit():
         if '@' in sign_in_form.username_or_email.data:
             user = session.query(User).filter(User.email == sign_in_form.username_or_email.data).first()
         else:
             user = session.query(User).filter(User.username == sign_in_form.username_or_email.data).first()
-            
+
         if user.check_password(sign_in_form.password.data):
             login_user(user, remember=sign_in_form.remember_me.data)
-            return redirect("/feed")
+            return redirect(url_for("feed"))
         
     return render_template(
         "sign-up-in.html", sign_up_form=sign_up_form, sign_in_form=sign_in_form
