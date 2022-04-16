7aa30d13b25ae46fe45633db6952d64db9a9a69a
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-16 19:01:44 +0300

    add column "preview_path" to videos table

--- app/data/models/videos.py
+++ app/data/models/videos.py
@@ -9,6 +9,7 @@ class Video(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     video_path = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    preview_path = sqlalchemy.Column(sqlalchemy.String, nullable=False)
 
     author_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"), nullable=False)
     author = orm.relation("User")
