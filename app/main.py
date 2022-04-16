494186e422fd551e5b33db266f283a0acd4d892e
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-16 17:18:17 +0300

    del unnecessary imports

--- app/main.py
+++ app/main.py
@@ -1,19 +1,5 @@
-from flask import (
-    Flask,
-    render_template,
-    redirect,
-    request,
-    jsonify,
-    abort,
-    make_response,
-)
-from flask_login import (
-    LoginManager,
-    login_required,
-    login_user,
-    logout_user,
-    current_user,
-)
+from flask import Flask
+from flask_login import LoginManager
 
 from data import db_session
 from data.models.users import User
@@ -30,7 +16,6 @@ login_manager.init_app(app)
 def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(user_id)
-    # return User.query.get(user_id)
 
 
 from data.routes import *
