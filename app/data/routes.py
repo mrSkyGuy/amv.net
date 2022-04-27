03af79f986a8b70e025fdcbb4c6bd4c493b0ad8b
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-27 18:54:03 +0300

    change template params

--- app/data/routes.py
+++ app/data/routes.py
@@ -18,6 +18,7 @@ def root():
     return redirect(url_for("feed"))
 
 
+# На данной странице расположена лента. Иначе говоря, это главная
 @app.route("/feed")
 def feed():
     # Получаем текущую ленту
@@ -30,49 +31,49 @@ def feed():
         session["current_video_index"] = 0
     current_video_index = session["current_video_index"]
 
+    # Текущее видео
     current_video = videos[
         feed_videos_indexes[current_video_index % len(feed_videos_indexes)]
     ]
+
+    # Предыдущее видео. Если мы в начале ленты, то его нет - None
     previous_video = None
     if current_video_index > 0:
         previous_video = videos[
             feed_videos_indexes[(current_video_index - 1) % len(feed_videos_indexes)]
         ]
+
+    # Следующее видео
     next_video = videos[
         feed_videos_indexes[(current_video_index + 1) % len(feed_videos_indexes)]
     ]
 
-    return render_template(
-        "feed.html",
-        previous_video_preview_path=previous_video.preview_path
-        if not (previous_video is None)
-        else "None",
 
-        current_video_content_path=current_video.video_path,
-        current_video_preview_path=current_video.preview_path,
-        current_video_description=current_video.description,
-        is_video_liked=(current_video in current_user.liked_videos)
-        if current_user.is_authenticated
-        else False,
-        
-        author_avatar_path=current_video.author.avatar_image,
-        author_username=current_video.author.username,
-        author_subscribers_count=len(current_video.author.followers),
-        author_videos_count=len(current_video.author.videos),
-        video_views_count=current_video.views_count,
-        video_likes_count=len(current_video.likes),
-        video_comments_count=current_video.comments_count,
-        next_video_preview_path=next_video.preview_path,
+    return render_template(
+        "feed.html", 
+        current_video=current_video, 
+        previous_video=previous_video,
+        next_video=next_video,
+        is_video_liked=len(
+            list(
+                filter(
+                    lambda video: video.id == current_video.id, 
+                    current_user.liked_videos
+                )
+            )
+        ) == 1  # Есть ли текущее видео в лайкнутых видео пользователем
+            if current_user.is_authenticated
+            else False,
     )
 
 
+# Страница регистрации и входа
 @app.route("/sign_up_in", methods=["GET", "POST"])
 def sign_up_in():
     sign_up_form = SignUpForm()
     sign_in_form = SignInForm()
 
     sess = create_session()
-    # if sign_up_form.validate_on_submit():
     if sign_up_form.submit_sign_up.data and sign_up_form.validate():
         # Из-за того, что на странице 2 формы нужно использовать такую проверку сабмита.
         # Чтобы при нажатии на один сабмит, второй не сработал и не проверилл свою форму
@@ -85,7 +86,6 @@ def sign_up_in():
 
         return redirect(url_for("sign_up_in") + "?sign=in")
 
-    # if sign_in_form.validate_on_submit():
     if sign_in_form.submit_sign_in.data and sign_in_form.validate():
         if "@" in sign_in_form.username_or_email.data:
             user = (
@@ -96,9 +96,7 @@ def sign_up_in():
         else:
             user = (
                 sess.query(User)
-                .filter(
-                    User.username == sign_in_form.username_or_email.data
-                )
+                .filter(User.username == sign_in_form.username_or_email.data)
                 .first()
             )
 
@@ -110,15 +108,19 @@ def sign_up_in():
         "sign-up-in.html",
         sign_up_form=sign_up_form,
         sign_in_form=sign_in_form,
-        which_sign=request.args.get("sign", "up", str),
+        which_sign=request.args.get("sign", "up", str),  
+        # which_sign - нужно, чтобы 
+        # если пользователь, например, нажал на sign IN, то его сразу же 
+        # перенаправило на нужный отдел
     )
 
 
+# Страница пользователя
 @app.route("/<username>")
 def users(username):
     session = create_session()
     user = session.query(User).filter(User.username == username).first()
     if user is None:
         abort(404)
-    
-    return render_template("user.html", user=user, user_videos_count=len(user.videos))
+
+    return render_template("user.html", user=user)
