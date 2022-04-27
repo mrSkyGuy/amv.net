1f21705c1fa3a84b704c7ca882248ed65af9cbf3
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-27 19:12:12 +0300

    add comments

--- app/static/scripts/like-button.js
+++ app/static/scripts/like-button.js
@@ -3,6 +3,7 @@ const likesCount = document.querySelector(".likes__count")
 
 
 function sendRequest(method, url, body = null) {
+    // Функция для отправки запросов на сервер
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest()
         xhr.open(method, url)
@@ -32,10 +33,15 @@ function sendRequest(method, url, body = null) {
 document.addEventListener("DOMContentLoaded", () => {
     likeButton.addEventListener('click', () => {
         if (likeButton.classList.contains("is-active")) {
+            // Если пользователь убирает лайк, то отправляем на сервер информацию о том,
+            // чтобы он убрал лайк
             const requestForDislikeVideo = sendRequest('GET', "/ajax/dislike_current_video")
+
+            // Я решил сделать так, чтобы лайк мог поставить любой. Но, чтобы он 
+            // учитывался, нужно авторизоваться
             requestForDislikeVideo.then(data => {
+                likeButton.classList.remove("is-active")
                 if (data["success"]) {
-                    likeButton.classList.remove("is-active")
                     likesCount.textContent = data["current_likes"]
                 } else {
                     // Показывать сообщение, что лайкать могут только авторизованные пользователи
@@ -44,8 +50,8 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             const requestForLikeVideo = sendRequest('GET', "/ajax/like_current_video")
             requestForLikeVideo.then(data => {
+                likeButton.classList.add("is-active")
                 if (data["success"]) {
-                    likeButton.classList.add("is-active")
                     likesCount.textContent = data["current_likes"]
                 } else {
                     // Показывать сообщение, что лайкать могут только авторизованные пользователи
