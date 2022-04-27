cafa5dfad626809af495924aaea568ecdb782799
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-27 18:08:08 +0300

    correct misstake

--- app/data/api/additional_funcs.py
+++ app/data/api/additional_funcs.py
@@ -3,7 +3,8 @@ from ..db_session import create_session
 
 
 def abort_if_item_not_found(item_id, item_class):
+    """ 404, если элемент не был найден """
     session = create_session()
-    news = session.query(item_class).get(item_id)
-    if not news:
+    item = session.query(item_class).get(item_id)
+    if not item:
         abort(404, success=False, message=f"{item_class.__name__} {item_id} not found")
