3bb03b4bc719556297d40f4d7c33957ada3df612
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-04-27 19:14:43 +0300

    add comments

--- app/static/scripts/switch-tab.js
+++ app/static/scripts/switch-tab.js
@@ -1,3 +1,5 @@
+// Данный код переключает вкладки на странице регистрации и входа
+
 const tabs = document.querySelectorAll('.button-sign')
 const indicator = document.querySelector('.selected-tab-indicator')
 const pages = [
@@ -12,25 +14,24 @@ document.addEventListener('DOMContentLoaded', () => {
             selectedTab.parentNode.children, selectedTab
         )
         indicator.style.transform = `translateX(calc( ${indicatorWidth} * ${tabIndex} + ${10 / 2 * tabIndex}px))`
+
+        tabs.forEach(tab => tab.addEventListener('click', e => {
+            const tab = e.target
+            const indicatorWidth = getComputedStyle(indicator).getPropertyValue('width')
+            const tabIndex = Array.prototype.indexOf.call(tab.parentNode.children, tab)
+            
+            if (tabIndex == 0) {
+                window.history.replaceState({}, '', '/sign_up_in?sign=up')  // Меняем url
+                pages[0].classList.add('current-page')
+                pages[1].classList.remove('current-page')
+            } else if (tabIndex == 1) {
+                window.history.replaceState({}, '', '/sign_up_in?sign=in')
+                pages[1].classList.add('current-page')
+                pages[0].classList.remove('current-page')
+            }
+            indicator.style.transform = `translateX(calc( ${indicatorWidth} * ${tabIndex} + ${10 / 2 * tabIndex}px))`  // 10 - tab-space из sign-up-in.scss файла
+            indicator.style.width = `calc(${indicatorWidth} + 2%)`
+            setTimeout(() => indicator.style.width = indicatorWidth, 100)
+        }))
     }, {once: true}
 )
-
-tabs.forEach(tab => tab.addEventListener('click', e => {
-    const tab = e.target
-    const indicatorWidth = getComputedStyle(indicator).getPropertyValue('width')
-    const tabIndex = Array.prototype.indexOf.call(tab.parentNode.children, tab)
-    
-    if (tabIndex == 0) {
-        // window.location.href = `http://"${window.location.domain}"/sign_up_in?sign=up`;
-        window.history.replaceState({}, '', '/sign_up_in?sign=up')
-        pages[0].classList.add('current-page')
-        pages[1].classList.remove('current-page')
-    } else if (tabIndex == 1) {
-        window.history.replaceState({}, '', '/sign_up_in?sign=in')
-        pages[1].classList.add('current-page')
-        pages[0].classList.remove('current-page')
-    }
-    indicator.style.transform = `translateX(calc( ${indicatorWidth} * ${tabIndex} + ${10 / 2 * tabIndex}px))`  // 10 - tab-space из sign-up-in.scss файла
-    indicator.style.width = `calc(${indicatorWidth} + 2%)`
-    setTimeout(() => indicator.style.width = indicatorWidth, 100)
-}))
